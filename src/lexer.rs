b35e11ff2de43c0b340c1c3a8a7ebba599660c60
bfoggo <brandon.foggo@habitat.energy>
2023-05-30 21:36:55 -0500

    initial setup

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,15 @@
+use crate::errors::LexerError;
+use std::iter::Peekable;
+use std::str::Chars;
+
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     EOF,
-    INVALID,
     NEWLINE,
     NUMBER,
     IDENT,
     STRING,
+    WHITESPACE,
 
     // Keywords
     PRINT,
@@ -26,7 +31,6 @@ pub enum Token {
     AND,
     OR,
     NOT,
-    IN,
     IS,
 
     // Operators
@@ -43,8 +47,52 @@ pub enum Token {
     GTEQ,
 }
 
-pub struct Lexer {
-    source: String,
-    position: usize,
+pub struct Lexer<'a> {
+    source: Peekable<Chars<'a>>,
     character: char,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: Peekable<Chars<'a>>) -> Lexer<'a> {
+        let mut lexer = Lexer {
+            source,
+            character: '\0',
+        };
+        lexer.next_char();
+        lexer
+    }
+
+    pub fn get_character(&self) -> &char {
+        &self.character
+    }
+
+    pub fn next_char(&mut self) -> char {
+        self.character = self.source.next().unwrap_or('\0');
+        self.character
+    }
+
+    pub fn peek(&mut self) -> &char {
+        self.source.peek().unwrap_or(&'\0')
+    }
+    pub fn abort(&self, message: &str) -> ! {
+        panic!("Lexing error: {}", message);
+    }
+    pub fn get_token(&self) -> Result<Token, LexerError> {
+        match self.character {
+            '\0' => Ok(Token::EOF),
+            '\n' => Ok(Token::NEWLINE),
+            '0'..='9' => Ok(Token::NUMBER),
+            'a'..='z' | 'A'..='Z' | '_' => Ok(Token::IDENT),
+            '"' => Ok(Token::STRING),
+            '+' => Ok(Token::PLUS),
+            '-' => Ok(Token::MINUS),
+            '*' => Ok(Token::ASTERISK),
+            '/' => Ok(Token::SLASH),
+            '=' => Ok(Token::EQ),
+            '<' => Ok(Token::LT),
+            '>' => Ok(Token::GT),
+            ' ' | '\t' => Ok(Token::WHITESPACE),
+            _ => Err(LexerError::InvalidToken(self.character)),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod errors;
+mod lexer;
+
+use errors::LexerError;
+use lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let source = "let x = 5 + 5 * 2";
+    let mut lexer = Lexer::new(source.chars().peekable());
+
+    while lexer.get_character().clone() != '\0' {
+        println!(
+            "{:?} - {:?}",
+            lexer.get_character(),
+            lexer.get_token().unwrap()
+        );
+        lexer.next_char();
+    }
 }
