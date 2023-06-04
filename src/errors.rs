a74d8235d7b11de0dfa1c44f810712b986dfd7eb
bfoggo <brandon.foggo@habitat.energy>
2023-06-04 10:43:58 -0500

    added parser errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,24 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum LexerError {
-    #[error("Invalid token: {0}")]
-    InvalidToken(String),
-    #[error("Invalid sequence token: {0}")]
-    InvalidBufferToken(String),
-    #[error("Tried to manipulate an empty buffer")]
-    EmptyBuffer,
-    #[error("Tried to extend the buffer past its source")]
-    BufferOverflow,
+pub enum LexerError {}
+
+#[derive(Debug, Error)]
+pub enum LetStatementError {
+    #[error("Let statements need an equal sign")]
+    NoEqualSign,
+    #[error("Let statements need an identifier")]
+    NoIdentifier,
+    #[error("Let statements need a semicolon")]
+    NoSemicolon,
+    #[error("Let statements need a value")]
+    NoValue,
+}
+
+#[derive(Debug, Error)]
+pub enum ParserError {
+    #[error("Invalid token")]
+    InvalidToken,
+    #[error("Let statement error: {0}")]
+    LetStatementError(LetStatementError),
 }
