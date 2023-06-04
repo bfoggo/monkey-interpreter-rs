4cf5b6648ae2cdf76b00fca9de1cff6e582f4554
bfoggo <brandon.foggo@habitat.energy>
2023-06-04 11:41:30 -0500

    Implemented a basic parser that can do let statements with literal expressions

---
+++ src/parser.rs
@@ -0,0 +1,102 @@
+use crate::errors::{ExpressionError, LetStatementError, ParserError, UndefinedBehaviorError};
+use crate::lexer::Token;
+use std::iter::Peekable;
+use std::vec::IntoIter;
+
+struct Identifier(String);
+struct LiteralExpression(String);
+
+enum Expression {
+    Literal(LiteralExpression),
+}
+
+struct LetStatement {
+    token: Token,
+    name: Identifier,
+    value: Expression,
+}
+
+enum Statement {
+    Let(LetStatement),
+}
+
+struct Program {
+    statements: Vec<Statement>,
+}
+struct Parser {
+    tokens: Peekable<IntoIter<Token>>,
+    current: usize,
+}
+
+impl Parser {
+    fn new(tokens: Vec<Token>) -> Parser {
+        Parser {
+            tokens: tokens.into_iter().peekable(),
+            current: 0,
+        }
+    }
+
+    fn parse(&mut self) -> Result<Program, ParserError> {
+        let mut program = Program {
+            statements: Vec::new(),
+        };
+        while let Some(token) = self.tokens.peek() {
+            match token {
+                Token::LET => {
+                    let statement = self.parse_let_statement()?;
+                    program.statements.push(Statement::Let(statement));
+                }
+                _ => {}
+            }
+        }
+        Ok(program)
+    }
+
+    fn parse_identifier(&mut self) -> Result<Identifier, UndefinedBehaviorError> {
+        let token = self.tokens.next().unwrap();
+        match token {
+            Token::IDENT(identifier) => {
+                let token = self.tokens.next().unwrap();
+                Ok(Identifier(identifier))
+            }
+            _ => {
+                return Err(UndefinedBehaviorError::InvalidParse(
+                    token,
+                    "identifier".to_string(),
+                ))
+            }
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Result<LetStatement, LetStatementError> {
+        let token = self.tokens.next().unwrap();
+        let next_token = self.tokens.peek().unwrap();
+        let mut identifier;
+        match next_token {
+            Token::IDENT(_) => {
+                identifier = self.parse_identifier()?;
+            }
+            _ => return Err(LetStatementError::NoIdentifier),
+        }
+        let check_eq_sign = self.tokens.peek().unwrap();
+        if *check_eq_sign != Token::EQ {
+            return Err(LetStatementError::NoEqualSign);
+        } else {
+            self.tokens.next().unwrap();
+        }
+        let value = self.parse_expression()?;
+        Ok(LetStatement {
+            token,
+            name: identifier,
+            value,
+        })
+    }
+
+    fn parse_expression(&mut self) -> Result<Expression, ExpressionError> {
+        let token = self.tokens.next().unwrap();
+        match token {
+            Token::NUMBER(num) => Ok(Expression::Literal(LiteralExpression(num))),
+            _ => Err(ExpressionError::InvalidExpression),
+        }
+    }
+}
