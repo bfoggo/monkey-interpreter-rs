5dfd949e6cbd1c8804faecd4a70245a75817bad9
bfoggo <brandon.foggo@habitat.energy>
2023-06-03 23:11:49 -0500

    refactored lexer to a single iterator with no buffer (peaks and enum constructions instead)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,168 +1,152 @@
 use crate::errors::LexerError;
-use std::{
-    fmt::{Debug, Formatter},
-    iter::Peekable,
-    str::Chars,
-};
+use std::{fmt::Debug, iter::Peekable, str::Chars};
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Token {
+pub enum SingleToken {
     EOF,
     NEWLINE,
-    WHITESPACE,
-    NUMBER(String),
-    IDENT(String),
     SEMICOLON,
-
-    // Keywords
-    PRINT,
-    LET,
-    IF,
-    ELSE,
-    ELIF,
-    WHILE,
-    FOR,
-    CONTINUE,
-    BREAK,
-    RETURN,
-    FUNKY,
-    TRUE,
-    FALSE,
-    NULL,
-    AND,
-    OR,
-    NOT,
-    IS,
-
-    // Operators
-    EQ,
     PLUS,
     MINUS,
     ASTERISK,
-    SLASH,
-    EQEQ,
-    NOTEQ,
-    LT,
-    LTEQ,
-    GT,
-    GTEQ,
-
-    // Delimiters
+    DOT,
+    COMMA,
     LPAREN,
     RPAREN,
     LBRACE,
     RBRACE,
-    COMMA,
-    DOT,
+    INVALID,
 }
 
-struct CharacterBuffer<'a> {
-    origin: &'a str,
-    position: usize,
-    size: usize,
+#[derive(Debug, PartialEq, Clone)]
+enum ComposableToken {
+    NUMBER(String),
+    IDENT(String),
+    EQ,
+    NOT,
+    LT,
+    GT,
 }
 
-impl<'a> Debug for CharacterBuffer<'a> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("CharacterBuffer")
-            .field("as_str", &self.as_ref())
-            .finish()
-    }
+#[derive(Debug, PartialEq, Clone)]
+enum SingleOrComposable {
+    Single(SingleToken),
+    Composable(ComposableToken),
 }
 
-impl<'a> AsRef<str> for CharacterBuffer<'a> {
-    fn as_ref(&self) -> &'a str {
-        &self.origin[self.position..self.position + self.size]
-    }
+#[derive(Debug, PartialEq, Clone)]
+pub enum CompositeToken {
+    NUMBER(String),
+    IDENT(String),
+    EQ,
+    NOT,
+    LT,
+    GT,
+    EQEQ,
+    NOTEQ,
+    LTEQ,
+    GTEQ,
 }
 
-impl<'a> CharacterBuffer<'a> {
-    fn new(origin: &'a str) -> CharacterBuffer<'a> {
-        CharacterBuffer {
-            origin,
-            position: 0,
-            size: 0,
+#[derive(Debug, PartialEq, Clone)]
+enum IntermediateToken {
+    Continue(ComposableToken),
+    Finish(CompositeToken),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    Single(SingleToken),
+    Composite(CompositeToken),
+}
+
+impl Token {
+    fn check_single(character: &char) -> Option<SingleOrComposable> {
+        match character {
+            '\0' => Some(SingleOrComposable::Single(SingleToken::EOF)),
+            '\n' => Some(SingleOrComposable::Single(SingleToken::NEWLINE)),
+            ';' => Some(SingleOrComposable::Single(SingleToken::SEMICOLON)),
+            '+' => Some(SingleOrComposable::Single(SingleToken::PLUS)),
+            '-' => Some(SingleOrComposable::Single(SingleToken::MINUS)),
+            '*' => Some(SingleOrComposable::Single(SingleToken::ASTERISK)),
+            '.' => Some(SingleOrComposable::Single(SingleToken::DOT)),
+            ',' => Some(SingleOrComposable::Single(SingleToken::COMMA)),
+            '(' => Some(SingleOrComposable::Single(SingleToken::LPAREN)),
+            ')' => Some(SingleOrComposable::Single(SingleToken::RPAREN)),
+            '{' => Some(SingleOrComposable::Single(SingleToken::LBRACE)),
+            '}' => Some(SingleOrComposable::Single(SingleToken::RBRACE)),
+            '=' => Some(SingleOrComposable::Composable(ComposableToken::EQ)),
+            '!' => Some(SingleOrComposable::Composable(ComposableToken::NOT)),
+            '<' => Some(SingleOrComposable::Composable(ComposableToken::LT)),
+            '>' => Some(SingleOrComposable::Composable(ComposableToken::GT)),
+            '0'..='9' => Some(SingleOrComposable::Composable(ComposableToken::NUMBER(
+                character.to_string(),
+            ))),
+            'a'..='z' | 'A'..='Z' | '_' => Some(SingleOrComposable::Composable(
+                ComposableToken::IDENT(character.to_string()),
+            )),
+            ' ' | '\t' => None,
+            _ => Some(SingleOrComposable::Single(SingleToken::INVALID)),
         }
     }
-
-    fn push(&mut self) -> Result<(), LexerError> {
-        self.size += 1;
-        Ok(())
-    }
-
-    fn skip(&mut self) -> Result<(), LexerError> {
-        self.position += 1;
-        Ok(())
-    }
-
-    fn clear(&mut self) {
-        self.position += self.size;
-        self.size = 0;
-    }
-
-    fn is_empty(&self) -> bool {
-        self.size == 0
-    }
-
-    fn is_single(&self) -> bool {
-        self.size == 1
-    }
-
-    fn parse(&mut self) -> Result<Option<Token>, LexerError> {
-        if self.is_empty() {
-            return Ok(None);
-        }
-        if self.is_single() {
-            let rtoken = Lexer::parse_single(
-                &self.origin[self.position..self.position + 1]
-                    .chars()
-                    .next()
-                    .unwrap(),
-            );
-            self.clear();
-            return rtoken;
-        }
-        let as_str = self.as_ref();
-        let rtoken = match as_str {
-            "print" => Ok(Some(Token::PRINT)),
-            "let" => Ok(Some(Token::LET)),
-            "if" => Ok(Some(Token::IF)),
-            "else" => Ok(Some(Token::ELSE)),
-            "elif" => Ok(Some(Token::ELIF)),
-            "while" => Ok(Some(Token::WHILE)),
-            "for" => Ok(Some(Token::FOR)),
-            "continue" => Ok(Some(Token::CONTINUE)),
-            "break" => Ok(Some(Token::BREAK)),
-            "return" => Ok(Some(Token::RETURN)),
-            "funky" => Ok(Some(Token::FUNKY)),
-            "true" => Ok(Some(Token::TRUE)),
-            "false" => Ok(Some(Token::FALSE)),
-            "null" => Ok(Some(Token::NULL)),
-            "and" => Ok(Some(Token::AND)),
-            "or" => Ok(Some(Token::OR)),
-            "not" => Ok(Some(Token::NOT)),
-            "is" => Ok(Some(Token::IS)),
-            "==" => Ok(Some(Token::EQEQ)),
-            "!=" => Ok(Some(Token::NOTEQ)),
-            "<=" => Ok(Some(Token::LTEQ)),
-            ">=" => Ok(Some(Token::GTEQ)),
-            "" => Ok(None),
-            digits if digits.chars().all(|c| c.is_digit(10)) => {
-                Ok(Some(Token::NUMBER(as_str.to_string())))
+    fn check_termination(
+        composable: IntermediateToken,
+        terminating_character: &char,
+    ) -> IntermediateToken {
+        match composable {
+            IntermediateToken::Finish(composite_token) => {
+                IntermediateToken::Finish(composite_token)
             }
-            letters if letters.chars().all(|c| c.is_alphabetic()) => {
-                Ok(Some(Token::IDENT(as_str.to_string())))
+            IntermediateToken::Continue(ComposableToken::NUMBER(curr_number)) => {
+                match terminating_character {
+                    '0'..='9' => IntermediateToken::Continue(ComposableToken::NUMBER(
+                        curr_number + &terminating_character.to_string(),
+                    )),
+                    _ => IntermediateToken::Finish(CompositeToken::NUMBER(curr_number)),
+                }
             }
-            _ => Err(LexerError::InvalidBufferToken(as_str.to_string())),
-        };
-        self.clear();
-        rtoken
+            IntermediateToken::Continue(ComposableToken::IDENT(curr_ident)) => {
+                match terminating_character {
+                    'a'..='z' | 'A'..='Z' | '_' | '0'..='9' => IntermediateToken::Continue(
+                        ComposableToken::IDENT(curr_ident + &terminating_character.to_string()),
+                    ),
+                    _ => IntermediateToken::Finish(CompositeToken::IDENT(curr_ident)),
+                }
+            }
+            IntermediateToken::Continue(ComposableToken::EQ) => match terminating_character {
+                '=' => IntermediateToken::Finish(CompositeToken::EQEQ),
+                _ => IntermediateToken::Finish(CompositeToken::EQ),
+            },
+            IntermediateToken::Continue(ComposableToken::NOT) => match terminating_character {
+                '=' => IntermediateToken::Finish(CompositeToken::NOTEQ),
+                _ => IntermediateToken::Finish(CompositeToken::NOT),
+            },
+            IntermediateToken::Continue(ComposableToken::LT) => match terminating_character {
+                '=' => IntermediateToken::Finish(CompositeToken::LTEQ),
+                _ => IntermediateToken::Finish(CompositeToken::LT),
+            },
+            IntermediateToken::Continue(ComposableToken::GT) => match terminating_character {
+                '=' => IntermediateToken::Finish(CompositeToken::GTEQ),
+                _ => IntermediateToken::Finish(CompositeToken::GT),
+            },
+        }
+    }
+}
+
+impl From<SingleToken> for Token {
+    fn from(single_token: SingleToken) -> Self {
+        Token::Single(single_token)
+    }
+}
+
+impl From<CompositeToken> for Token {
+    fn from(final_composite_token: CompositeToken) -> Self {
+        Token::Composite(final_composite_token)
     }
 }
 
 #[derive(Debug)]
 struct Lexer<'a> {
-    buffer: CharacterBuffer<'a>,
     char_iter: Peekable<Chars<'a>>,
     curr_character: char,
 }
@@ -170,7 +154,6 @@ struct Lexer<'a> {
 impl<'a> Lexer<'a> {
     fn new(source: &'a str) -> Lexer<'a> {
         let mut lexer = Lexer {
-            buffer: CharacterBuffer::new(source),
             char_iter: source.chars().peekable(),
             curr_character: '\0',
         };
@@ -190,68 +173,36 @@ impl<'a> Lexer<'a> {
         self.curr_character == '\0'
     }
 
-    fn finish(&mut self) -> Result<Option<Token>, LexerError> {
-        self.buffer.parse()
-    }
-
-    fn needs_buffer(character: &char) -> bool {
-        match character {
-            'a'..='z' | 'A'..='Z' | '_' | '0'..='9' => true,
-            '=' => true,
-            '<' => true,
-            '>' => true,
-            _ => false,
-        }
-    }
-    fn is_buffer_terminating(character: &char) -> bool {
-        match character {
-            '\0' | '\n' | ' ' | '\t' | '+' | '-' | '*' | '/' | '=' | '<' | '>' | ';' | '('
-            | ')' | '{' | '}' | ',' | '.' => true,
-            _ => false,
-        }
-    }
-    fn parse_single(character: &char) -> Result<Option<Token>, LexerError> {
-        match character {
-            '\0' => Ok(Some(Token::EOF)),
-            '\n' => Ok(Some(Token::NEWLINE)),
-            '+' => Ok(Some(Token::PLUS)),
-            '-' => Ok(Some(Token::MINUS)),
-            '*' => Ok(Some(Token::ASTERISK)),
-            '/' => Ok(Some(Token::SLASH)),
-            '=' => Ok(Some(Token::EQ)),
-            '<' => Ok(Some(Token::LT)),
-            '>' => Ok(Some(Token::GT)),
-            '0'..='9' => Ok(Some(Token::NUMBER(character.to_string()))),
-            'a'..='z' | 'A'..='Z' | '_' => Ok(Some(Token::IDENT(character.to_string()))),
-            ';' => Ok(Some(Token::SEMICOLON)),
-            '(' => Ok(Some(Token::LPAREN)),
-            ')' => Ok(Some(Token::RPAREN)),
-            '{' => Ok(Some(Token::LBRACE)),
-            '}' => Ok(Some(Token::RBRACE)),
-            ',' => Ok(Some(Token::COMMA)),
-            '.' => Ok(Some(Token::DOT)),
-            ' ' | '\t' => Ok(None),
-            _ => Err(LexerError::InvalidToken(character.clone())),
+    fn build_composite_token(
+        &mut self,
+        intermediate_token: IntermediateToken,
+    ) -> Result<Token, LexerError> {
+        let new_token = Token::check_termination(intermediate_token, &self.curr_character);
+        match new_token {
+            IntermediateToken::Finish(composite_token) => {
+                self.advance();
+                Ok(Token::from(composite_token))
+            }
+            IntermediateToken::Continue(_) => {
+                self.advance();
+                let final_token = self.build_composite_token(new_token)?;
+                Ok(Token::from(final_token))
+            }
         }
     }
 
     fn get_token(&mut self) -> Result<Option<Token>, LexerError> {
-        match self.curr_character {
-            character if Lexer::needs_buffer(&character) => {
-                while !self.is_end()
-                    && !Lexer::is_buffer_terminating(&self.char_iter.peek().unwrap_or(&'\0'))
-                {
-                    self.buffer.push()?;
-                    self.advance();
-                }
-                self.buffer.push()?;
-                let token = self.buffer.parse();
-                token
+        match Token::check_single(&self.curr_character) {
+            Some(SingleOrComposable::Single(single_token)) => {
+                self.advance();
+                Ok(Some(Token::from(single_token)))
             }
-            _ => {
-                let token = Lexer::parse_single(&self.curr_character);
-                self.buffer.skip()?;
-                token
+            Some(SingleOrComposable::Composable(composable_token)) => Ok(Some(
+                self.build_composite_token(IntermediateToken::Continue(composable_token))?,
+            )),
+            None => {
+                self.advance();
+                Ok(None)
             }
         }
     }
@@ -267,39 +218,5 @@ pub fn lex(source: &str) -> Result<Vec<Token>, LexerError> {
         }
         lexer.advance();
     }
-    let final_token = lexer.finish()?;
-    if let Some(final_token) = final_token {
-        tokens.push(final_token);
-    }
     Ok(tokens)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_buffer() {
-        let mut buffer = CharacterBuffer::new("hello world");
-        for i in 1..3 {
-            buffer.push().unwrap();
-            assert_eq!(buffer.size, i);
-        }
-        assert_eq!(buffer.as_ref(), "he");
-        let he_token = buffer.parse().unwrap().unwrap();
-        assert_eq!(he_token, Token::IDENT("he".to_string()));
-        assert_eq!(buffer.position, 2);
-        assert_eq!(buffer.size, 0);
-        for i in 1..6 {
-            buffer.push().unwrap();
-            assert_eq!(buffer.size, i);
-        }
-        assert_eq!(buffer.as_ref(), "llo w");
-        buffer.clear();
-        assert_eq!(buffer.position, 7);
-        for i in 1..4 {
-            buffer.push().unwrap();
-            assert_eq!(buffer.size, i);
-        }
-        assert!(buffer.push().is_err());
-    }
 }
